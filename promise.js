3002cb5a24ea661b80771eb5626fad4254e463c2
tonymadjid <tonymadjid@yahoo.com>
2022-06-12 21:47:21 +0700

    tugas promise

--- promise.js
+++ promise.js
@@ -1,8 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi) => {
+    try {
+        let theater1 = await promiseTheaterIXX();
+        let theater2 = await promiseTheaterVGC();
+        let theater = theater1.concat(theater2);
+        let total = theater.filter(em => em.hasil === emosi);
+        return total.length;
+    } catch (err) {
+        console.log(err);
+
+    }
+};
 
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
